379fcd2bb9066e23ab13d3d9b9f9c47bc8ab3f47
Piotr Filipiak <kiddhkane@gmail.com>
2022-08-07 21:38:53 +0200

    Added basic routes. Made a file for routers.

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,7 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.get("/api/goals", (req, res) => {
-  //   res.send("Get Goals");    simple
-  res.status(200).json({ message: "Get goals" });
-  //   you don't have to add the status manually, but you can
-});
+// because of this line, we don't need full path in goalRoutes.js
+app.use("/api/goals", require("./routes/goalRoutes"));
 
 app.listen(port, () => console.log(`Server started on port: ${port}`));
