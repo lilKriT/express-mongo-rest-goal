4f7cbbcf92cdf59f0ac5dd13b1bcd947546bbaef
Piotr Filipiak <kiddhkane@gmail.com>
2022-08-07 21:28:45 +0200

    Added a first route.

--- backend/server.js
+++ backend/server.js
@@ -4,4 +4,10 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.get("/api/goals", (req, res) => {
+  //   res.send("Get Goals");    simple
+  res.status(200).json({ message: "Get goals" });
+  //   you don't have to add the status manually, but you can
+});
+
 app.listen(port, () => console.log(`Server started on port: ${port}`));
