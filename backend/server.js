85691ded0723422d563100f7d2935606cb3b6a16
Piotr Filipiak <kiddhkane@gmail.com>
2022-08-07 22:12:47 +0200

    Added error middleware.

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+// adding middleware, so I can read the body of a request
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 // because of this line, we don't need full path in goalRoutes.js
 app.use("/api/goals", require("./routes/goalRoutes"));
+
+// overriding the error handler
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port: ${port}`));
