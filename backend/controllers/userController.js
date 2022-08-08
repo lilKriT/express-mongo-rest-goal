777d048bf3e9a6262f2bf1d38dbd4132d41b85ff
Piotr Filipiak <kiddhkane@gmail.com>
2022-08-08 18:29:50 +0200

    Added protection.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -68,9 +68,15 @@ const loginUser = asyncHandler(async (req, res) => {
 
 // @desc Get User data
 // @route GET /api/users/me
-// @acccess Public
+// @acccess Private
 const getMe = asyncHandler(async (req, res) => {
-  res.json({ message: "User data" });
+  const { _id, name, email } = await User.findById(req.user.id);
+
+  res.status(200).json({
+    id: _id,
+    name,
+    email,
+  });
 });
 
 // Generate JWT (JSON Web Token)
