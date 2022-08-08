b4d2d422728c935d5f1926f1ab8def6964a8ae15
Piotr Filipiak <kiddhkane@gmail.com>
2022-08-08 17:28:11 +0200

    Added authentication.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -35,6 +35,7 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -57,6 +58,7 @@ const loginUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -70,6 +72,14 @@ const loginUser = asyncHandler(async (req, res) => {
 const getMe = asyncHandler(async (req, res) => {
   res.json({ message: "User data" });
 });
+
+// Generate JWT (JSON Web Token)
+const generateToken = (id) => {
+  // this creates a token. takes a payload
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
 
 module.exports = {
   registerUser,
