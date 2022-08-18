e6fde35c69416251ec382c27ae4edbe8dcb88fff
Piotr Filipiak <kiddhkane@gmail.com>
2022-08-18 22:08:18 +0200

    Prepared for Heroku deployment.

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
@@ -16,6 +17,19 @@ app.use(express.urlencoded({ extended: false }));
 // because of this line, we don't need full path in goalRoutes.js
 app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
+
+// Serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
 
 // overriding the error handler
 app.use(errorHandler);
